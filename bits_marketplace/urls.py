8b3e00b1f33ee1c748c7eaf5c8af9ed3eaaf71d9
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-10-16 13:11:06 +0530

    preparing for deployment

--- bits_marketplace/urls.py
+++ bits_marketplace/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -27,6 +30,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
     path('', include('item.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
