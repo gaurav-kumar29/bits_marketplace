512a288e23017bfa71ef91b62682153eb93b0947
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-10-16 16:35:57 +0530

    updated settings.py

--- bits_marketplace/settings.py
+++ bits_marketplace/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w^ljgd33@n-jc*y0ls+^l&a2e*+&2fmv41f30mb@*jhxfs9w24'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bitsmarketplace.herokuapp.com']
 
