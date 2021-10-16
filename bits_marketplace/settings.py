589072f7834de71b3e4d4a1af656acfa75eab5e5
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-10-16 16:13:55 +0530

    removed gitignore

--- bits_marketplace/settings.py
+++ bits_marketplace/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w^ljgd33@n-jc*y0ls+^l&a2e*+&2fmv41f30mb@*jhxfs9w24
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bitsmarketplace.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'bitsmarketplace.herokuapp.com']
 
 
 # Application definition
